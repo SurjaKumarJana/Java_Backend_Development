7b263c606d2267df92fe2f7baec0c03c31089bc0
Surja Kumar Jana <learn.surja01@gmail.com>
2025-06-21 20:20:24 +0530

    Basics of java added

---
+++ java_fundamentals/src/com/fundamental/basics/HelloWorldProgram.java
@@ -0,0 +1,10 @@
+package com.fundamental.basics;
+
+public class HelloWorldProgram {
+    // the below line is fixed in any java program
+    // we can modify this below line we have to write it as it is ...
+    public static void main(String[] args) {
+        // output
+        System.out.println("Hello ");
+    }
+}
