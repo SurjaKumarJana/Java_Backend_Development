0e8441fbd5f60791eabdab4695e3c6848f80765a
Surja Kumar Jana <learn.surja01@gmail.com>
2025-07-13 18:26:46 +0530

    Collection Framework : keyword analyser project

---
+++ collectionFramwork/src/com/surja/keywordAnalyser/KeywordAnalyserInterface.java
@@ -0,0 +1,35 @@
+package com.surja.keywordAnalyser;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+/*
+this is the contract that is share between two developer
+one is developing the search service
+another one is taking care of implementation
+
+ */
+
+
+public interface KeywordAnalyserInterface {
+    /*
+     our search function will have mainly two functionality
+        one is record the keyword
+        another one is to get all the stored keyword
+     */
+    /*
+    this method is to record the keyword
+     */
+    void recordKeyword(String keyword);
+    /*
+    this method is to get all the recorded Keywords
+     */
+    List<String> getKeyword();
+
+    /*
+    this is a default method
+     */
+    default Map<String, Long> getKeywordFrq(){
+        return null;
+    }
+}
---
+++ collectionFramwork/src/com/surja/keywordAnalyser/MockKeywordAnalyserImp.java
@@ -0,0 +1,25 @@
+
+/**
+ * author: Developer A
+ *
+ * this is a mock interface that Developer A created to work on his part
+ * this is basically a dummy implementation that does nothing.
+ */
+
+package com.surja.keywordAnalyser;
+
+import java.util.List;
+
+public class MockKeywordAnalyserImp implements KeywordAnalyserInterface{
+    @Override
+    public void recordKeyword(String keyword) {
+
+    }
+
+    @Override
+    public List<String> getKeyword() {
+        return null;
+    }
+
+
+}
