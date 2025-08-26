81db4077ff7207ce7b73c1fa694d0da53eb9a19f
Surja Kumar Jana <learn.surja01@gmail.com>
2025-08-27 04:43:47 +0530

    Micro Services :Rest Template added

---
+++ micro_Services/restTemplate-feignClient-demo/src/main/java/org/surja/restTemplate_feignClient_demo/RestTemplateFeignClientDemoApplication.java
@@ -0,0 +1,13 @@
+package org.surja.restTemplate_feignClient_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestTemplateFeignClientDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestTemplateFeignClientDemoApplication.class, args);
+	}
+
+}
